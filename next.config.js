becbbf0270849893b527fcd7a5ab9cb86cb05057
parkhyeonji <iamphj3@naver.com>
2023-09-08 16:00:31 +0900

    init : eslint, prettier 세팅

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    // styledComponents: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  experimental: {
+    // fontLoaders: [
+    //   { loader: '@next/font/google', options: { subsets: ['latin'] } },
+    // ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
